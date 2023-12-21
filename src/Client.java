c690a0a567cdab694b8077c6bed15375a3e94d55
pasquinucci.matteo <pasquinucci.matteo@gmail.com>
2023-12-21 10:22:04 +0100

    Add file client and server

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,26 @@
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-vier sei una merda umamana aa nnaauajinecuibhueiuiwfeu UYTYFUETUYFTF UYETFUTYfyutFEWYFITYUEWTFYU    WTFYUT  FEYFYU  WTFEUYTFYUTFYU TEW  WF  EWW Y   U
 
-}
+public class Client  extends Thread {
+        Socket socket;
+        public Client(Socket socket) { this.socket = socket; }
+        public void run() {
+            try {
+                BufferedReader bufferedReader =  new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedWriter bufferedWriter =  new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+                while (true) {
+                    String string = bufferedReader.readLine();
+                    bufferedWriter.write(string);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
