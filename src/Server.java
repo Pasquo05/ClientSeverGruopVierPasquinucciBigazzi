f97c5327594a278358b375c585048bdbf1ba681c
pasquinucci.matteo <pasquinucci.matteo@gmail.com>
2023-12-21 10:22:04 +0100

    Add file client and server

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+
+
 public class Server {
-
-
-
+    public Server(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("in attesa su " + port);
+                Socket socket = serverSocket.accept();
+                System.out.println("ricevuta connessione:"  + socket.getInetAddress() + ":" + socket.getPort());
+                (new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
+
